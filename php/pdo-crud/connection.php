0a7d88f823f7756fc310997808f522d06edfb229
Uğur Arıcı <m.ugurarici@gmail.com>
2017-07-31 20:50:10 +0300

    pdo-crud örneği eklenir

---
+++ php/pdo-crud/connection.php
@@ -0,0 +1,9 @@
+<?php
+// Veritabanı sunucusuna bağlanacağım
+
+$connection = new PDO("mysql:host=localhost;dbname=lyk2016_sis;charset=utf8", "root", "root");
+
+//	mysql:
+//		host=localhost;
+//		dbname=lyk2017_sis;
+//		charset=utf8
